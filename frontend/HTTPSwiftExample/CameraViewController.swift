eb4b81eb4d6a68133fe08b082517422afc903ede
Edward-jiang495 <54651927+Edward-jiang495@users.noreply.github.com>
2021-11-14 15:42:07 -0600

    basic camera usage and send method added

--- frontend/HTTPSwiftExample/CameraViewController.swift
+++ frontend/HTTPSwiftExample/CameraViewController.swift
@@ -8,58 +8,116 @@
 
 import UIKit
 
-class CameraViewController: UIViewController {
-    lazy var videoManager:VideoAnalgesic! = {
-//        create video analgesic
-        let tmpManager = VideoAnalgesic(mainView: self.view)
-        tmpManager.setCameraPosition(position: .front)
-        return tmpManager
+
+class CameraViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, URLSessionDelegate{
+    let SERVER_URL = "http://10.8.27.223:8000" // change this for your server name!!!
+    let operationQueue = OperationQueue()
+
+    var imagePicker: UIImagePickerController!
+    lazy var session: URLSession = {
+        let sessionConfig = URLSessionConfiguration.ephemeral
+        
+        sessionConfig.timeoutIntervalForRequest = 5.0
+        sessionConfig.timeoutIntervalForResource = 8.0
+        sessionConfig.httpMaximumConnectionsPerHost = 1
+        
+        return URLSession(configuration: sessionConfig,
+            delegate: self,
+            delegateQueue:self.operationQueue)
     }()
-    var cameraFront:Bool = true;
-    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.videoManager.setProcessingBlock(newProcessBlock: self.processImage)
-//        set running code
-        
-        if !videoManager.isRunning{
-            videoManager.start()
-//            start running
-        }
+
 
         // Do any additional setup after loading the view.
     }
-    override func viewDidDisappear(_ animated: Bool){
-        
-        DispatchQueue.main.async {
-            self.videoManager.shutdown()
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBAction func takePhoto(_ sender: UIButton) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            imagePicker.dismiss(animated: true, completion: nil)
+            imageView.image = info[.originalImage] as? UIImage
         }
-        super.viewDidDisappear(animated)
+    
+    @IBAction func sendToServer(_ sender: UIButton) {
+        let image = imageView.image
+        let baseURL = "\(SERVER_URL)/AddImage"
+        let postUrl = URL(string: "\(baseURL)")
+        
+        // create a custom HTTP POST request
+        var request = URLRequest(url: postUrl!)
+        
+        // data to send in body of post request (send arguments as json)
+        let jsonUpload:NSDictionary = ["image": image,
+                                       "id":0]
+        
+        
+        let requestBody:Data? = self.convertDictionaryToData(with:jsonUpload)
+        
+        request.httpMethod = "POST"
+        request.httpBody = requestBody
+        
+        let postTask : URLSessionDataTask = self.session.dataTask(with: request,
+            completionHandler:{(data, response, error) in
+                if(error != nil){
+                    if let res = response{
+                        print("Response:\n",res)
+                    }
+                }
+                else{
+                    let jsonDictionary = self.convertDataToDictionary(with: data)
+//
+//                    print(jsonDictionary["feature"]!)
+//                    print(jsonDictionary["label"]!)
+                }
+
+        })
+        
+        postTask.resume() // start the task
+        
+        
         
     }
-
-
-    @IBAction func toggleCamera(_ sender: UIButton) {
-        if cameraFront{
-            videoManager.setCameraPosition(position: .back)
-            cameraFront = !cameraFront
-        }
-        else{
-            videoManager.setCameraPosition(position: .front)
-            cameraFront = !cameraFront
+    
+    func convertDataToDictionary(with data:Data?)->NSDictionary{
+        do { // try to parse JSON and deal with errors using do/catch block
+            let jsonDictionary: NSDictionary =
+                try JSONSerialization.jsonObject(with: data!,
+                                              options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+            
+            return jsonDictionary
+            
+        } catch {
+            
+            if let strData = String(data:data!, encoding:String.Encoding(rawValue: String.Encoding.utf8.rawValue)){
+                            print("printing JSON received as string: "+strData)
+            }else{
+                print("json error: \(error.localizedDescription)")
+            }
+            return NSDictionary() // just return empty
         }
     }
     
-    func processImage(inputImage:CIImage) -> CIImage{
-        
-        //here we process the images
-        return inputImage
+    func convertDictionaryToData(with jsonUpload:NSDictionary) -> Data?{
+        do { // try to make JSON and deal with errors using do/catch block
+            let requestBody = try JSONSerialization.data(withJSONObject: jsonUpload, options:JSONSerialization.WritingOptions.prettyPrinted)
+            return requestBody
+        } catch {
+            print("json error: \(error.localizedDescription)")
+            return nil
+        }
     }
-    
-    
-
     /*
     // MARK: - Navigation
 
