13d72de69fff670f1255556d231a892abc3626f7
Nathan Gage <ngage@smu.edu>
2021-11-16 13:44:24 -0600

    __init__.py contains directories

--- backend/MLService/mlhandler.py
+++ backend/MLService/mlhandler.py
@@ -1,7 +1,8 @@
-# import tensorflow as tf
+import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers  # import Dense, Flatten, Conv2D, MaxPooling2D, Rescaling, RandomFlip
-from enum import Enum
+from tensorflow.keras import layers
+
+from backend.MLService import *
 
 # model parameters
 batch_size = 32
@@ -9,60 +10,48 @@ img_height = 75
 img_width = 75
 optimizer = keras.optimizers.Adam(learning_rate=0.0001)
 num_classes = 2
-base_image_directory = "../images/truth/"
-user_image_directory = "../images/user/"
 
 
-class ModelType(Enum):
-    INCEPTION_RESNET_V2 = 0
-    XCEPTION = 1
+def get_dataset_from_dir(images_dir: path) -> tf.data.Dataset:
+    return keras.preprocessing.image_dataset_from_directory(images_dir,
+                                                            label_mode="binary",
+                                                            image_size=(img_height, img_width),
+                                                            batch_size=batch_size,
+                                                            shuffle=True,
+                                                            crop_to_aspect_ratio=False)
 
 
-def get_dataset(dataset_directory: str) -> tuple:
-    """
-    Returns a tuple of (train_dataset, test_dataset)
+def get_dataset_from_types(model_type: ModelType, pretrain_type: PretrainType) -> tuple:
+    train_dir = None
 
-    Args:
-        dataset_directory: The directory containing train/ and test/
+    if model_type == ModelType.BASE:
+        train_dir = image_dirs[model_type]['train']
+    elif model_type == ModelType.USER:
+        train_dir = image_dirs[model_type][pretrain_type]
 
-    Returns:
-        train_dataset: A tf.data.Dataset object containing the training data
-        test_dataset: A tf.data.Dataset object containing the test data
-
-    """
-    if dataset_directory[-1] != '/':
-        dataset_directory += '/'
-
-    train_ds = keras.preprocessing.image_dataset_from_directory(dataset_directory + "train", label_mode="binary",
-                                                                image_size=(img_height, img_width),
-                                                                batch_size=batch_size, shuffle=True,
-                                                                crop_to_ascpect_ratio=True)
-
-    test_ds = keras.preprocessing.image_dataset_from_directory(dataset_directory + "test", label_mode="binary",
-                                                               image_size=(img_height, img_width),
-                                                               batch_size=batch_size, shuffle=True,
-                                                               crop_to_ascpect_ratio=True)
+    train_ds = get_dataset_from_dir(train_dir)
+    test_ds = get_dataset_from_dir(image_dirs[ModelType.BASE]['test'])
 
     return train_ds, test_ds
 
 
-def make_pretrainable_model(model_type: ModelType) -> keras.Model:
+def make_pretrainable_model(pretrain_type: PretrainType) -> keras.Model:
     """
     Generates a transfer learning model from a pretrained model
 
     Args:
-        model_type: base model to use for transfer learning
+        pretrain_type: base model to use for transfer learning
 
     Returns:
         model: A tf.keras.Model object
     """
 
     pretrained_models = {
-        ModelType.INCEPTION_RESNET_V2: keras.applications.inception_resnet_v2.InceptionResNetV2,
-        ModelType.XCEPTION: keras.applications.xception.Xception
+        PretrainType.INCEPTION_RESNET_V2: keras.applications.inception_resnet_v2.InceptionResNetV2,
+        PretrainType.XCEPTION: keras.applications.xception.Xception
     }
 
-    pretrained_model = pretrained_models[model_type](input_shape=(img_height, img_width, 3), include_top=False)
+    pretrained_model = pretrained_models[pretrain_type](input_shape=(img_height, img_width, 3), include_top=False)
     pretrained_model.trainable = False
 
     model = keras.Sequential([
@@ -85,47 +74,94 @@ def make_pretrainable_model(model_type: ModelType) -> keras.Model:
     return model
 
 
-def train_base_model(model_type: ModelType, epochs: int = 10):
-    model = make_pretrainable_model(model_type)
-    train_ds, test_ds = get_dataset(dataset_directory)
+def save_model(model: keras.Model, directory: path) -> None:
+
+
+
+    model.save(directory, overwrite=True, include_optimizer=True, save_format="tf")
+
+
+def load_model(directory: path) -> keras.Model:
+    try:
+        model = keras.models.load_model(directory, compile=True)
+        return model
+    except (IOError, ImportError):
+        return None
+
+
+def get_base_model(pretrain_type: PretrainType) -> keras.Model:
+    model = load_model(model_dirs[ModelType.BASE][pretrain_type])
+
+    if model is not None:
+        print("Using cached base model")
+
+        return model
+    else:
+        print("Training new base model")
+
+        model = make_pretrainable_model(pretrain_type)
+        train_ds, test_ds = get_dataset_from_types(ModelType.BASE, pretrain_type)
+
+        model.fit(train_ds, validation_data=test_ds, epochs=10)
+
+        print("Saving new base model")
+        save_model(model, model_dirs[ModelType.BASE][pretrain_type])
+
+        return model
+
+
+def get_user_model(pretrain_type: PretrainType):
+    model = load_model(model_dirs[ModelType.USER][pretrain_type])
+
+    if model is not None:
+        print("Using cached user model")
+
+        return model
+    else:
+        print("Using new base model")
+        model = get_base_model(pretrain_type)
+
+        print("Saving new user model")
+        save_model(model, model_dirs[ModelType.USER][pretrain_type])
+
+        return model
+
+
+def get_model(model_type: ModelType, pretrain_type: PretrainType) -> keras.Model:
+    if model_type == ModelType.BASE:
+        return get_base_model(pretrain_type)
+    elif model_type == ModelType.USER:
+        return get_user_model(pretrain_type)
+    else:
+        raise ValueError("Invalid model type")
+
+
+def train_user_model(model: keras.Model, pretrain_type: PretrainType, images: path, epochs=5) -> tuple:
+    train_ds = get_dataset_from_dir(images)
+    test_ds = get_dataset_from_dir(image_dirs[ModelType.BASE]['test'])
+
     history = model.fit(train_ds, validation_data=test_ds, epochs=epochs)
+
+    print("Saving updated user model")
+    save_model(model, model_dirs[ModelType.USER][pretrain_type])
 
     return model, history
 
 
-def train_model(model: keras.Model, epochs: int = 10):
-    train_ds, _ = get_dataset(user_image_directory)
-    _, test_ds = get_dataset(base_image_directory)
-    history = model.fit(train_ds, validation_data=test_ds, epochs=epochs)
+def get_prediction(model: keras.Model, image_path: path) -> float:
+    img = keras.utils.load_img(image_path, target_size=(img_height, img_width))
+    img_array = keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
 
-    return model, history
-
-
-def save_model():
-    pass
-
-
-def load_model():
-    pass
-
-
-def get_prediction():
-    pass
+    predictions = model.predict(img_array)
+    return predictions[0]
 
 
 def __main__():
-    from matplotlib import pyplot as plt
+    model = get_model(ModelType.USER, PretrainType.INCEPTION_RESNET_V2)
+    # train_model(model, image_dirs[ModelType.USER]['train'])
 
-    epochs = 5
-
-    cnn_model, cnn_history = train_base_model(ModelType.XCEPTION, epochs=epochs)
-    mlp_model, mlp_history = train_base_model(ModelType.INCEPTION_RESNET_V2, epochs=epochs)
-
-    acc = mlp_history.history['binary_accuracy']
-    val_acc = mlp_history.history['val_binary_accuracy']
-
-    loss = mlp_history.history['loss']
-    val_loss = mlp_history.history['val_loss']
+    print(get_prediction(model, path.join(image_dirs[ModelType.BASE]['train'], 'hot_dog', '133012.jpg')))
 
 
 if __name__ == '__main__':
